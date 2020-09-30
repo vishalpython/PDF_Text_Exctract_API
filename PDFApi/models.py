39c9014fefbe474c29ce363020419fcdcf0c6073
vishal <vishalgore889@gmail.com>
2020-09-30 13:05:30 +0530

    Model added

--- PDFApi/models.py
+++ PDFApi/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+
+class PFDInvoice(models.Model):
+    """Model that represent the invoice data"""
+    invoice_number = models.CharField(max_length=225)
+    Account = models.CharField(max_length=225)
+    descripition = models.CharField(max_length=500)
+    date = models.DateField()
+    phone_no = models.IntegerField()
+    address = models.TextField()
+    subtoatal = models.IntegerField()
+    shipping = models.IntegerField()
+    total    = models.IntegerField()
